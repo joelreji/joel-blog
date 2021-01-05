2771c6e74b0b24b766a3bd8e662a1ec78ee02140
joelreji <joelreji97@gmail.com>
2021-01-05 15:09:41 -0500

    Making some updates

--- joel_blog/urls.py
+++ joel_blog/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('portfolio/', include('portfolio.urls')),
     path('subscribe/', include('sub.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
