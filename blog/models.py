048c97e7b8bb1fd4cbae41f9f1f43fdbbd6856b7
joelreji <joelreji97@gmail.com>
2021-01-07 14:07:15 -0500

    Few updates to make admin much easier

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=225)
     description = models.TextField(max_length=100)
@@ -12,9 +15,14 @@ class Post(models.Model):
     categories = models.ManyToManyField('Category', related_name='posts')
     image = models.ImageField(upload_to='images/')
 
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     author = models.CharField(max_length=60)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.author
--- sub/models.py
+++ sub/models.py
@@ -8,5 +8,5 @@ class NewsUsers(models.Model):
     email = models.EmailField()
     date_added = models.DateField(auto_now_add=True)
 
-def __str__(self):
-            return self.email
+    def __str__(self):
+        return self.email
