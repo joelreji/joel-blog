70ad6dd23557fe52b44042a4989b06021dc587cb
joelreji <joelreji97@gmail.com>
2021-09-19 09:31:04 -0400

    big update to sub

--- joel_blog/settings.py
+++ joel_blog/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'portfolio',
     'blog',
     'sub',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,5 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
